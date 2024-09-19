5ad8abdc9b34c24aa928f51dad18d8fa8225b6d5
jass921026 <jass921026@gmail.com>
2024-09-19 09:17:33 +0800

    changed duplicate number reroll logic

--- Lab1/src/Top.sv
+++ Lab1/src/Top.sv
@@ -8,13 +8,12 @@ module Top (
 parameter S_IDLE = 1'b0;
 parameter S_PROC = 1'b1;
 
-logic 			state_r, 		state_w;		//Finite State Machine
-logic [15:0] 	random_number; 					//catch the output of LFSR
-logic [3:0] 	o_random_out_r, o_random_out_w;	//Stored Random Variable
-logic [3:0] 	last_random_r, 	last_random_w;	//Last Random Variable
-logic [10:0] 	clk_50hz_r, 	clk_50hz_w; 	//a slower clock runs at 50hz
-logic [19:0] 	accumulator_r, 	accumulator_w; 	//50hz clock internal variable
-logic [4:0]		rnd_gen_cnt_r, 	rnd_gen_cnt_w; 	//count how many times the random number is generated
+logic 			state_r, 			state_w;				// Finite State Machine
+logic [15:0] 	random_number; 							// Catch the output of LFSR
+logic [3:0] 	o_random_out_r, 	o_random_out_w;	// Stored Random Variable
+logic [10:0] 	clk_50hz_r, 		clk_50hz_w; 		// A slower clock runs at 50hz
+logic [19:0] 	accumulator_r, 	accumulator_w; 	// 50hz clock internal variable
+logic [4:0]		rnd_gen_cnt_r, 	rnd_gen_cnt_w; 	// Count how many times the random number is generated
 int rnd_gen_cnt_wait[35] = '{7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,8,9,10,11,12,14,16,18,20,23,26,30,35,40,50,50,50,50,50};
 
 lfsr lfsr_inst (
@@ -29,40 +28,33 @@ always_comb begin
 	// Default Values
 	state_w        	= state_r;
 	o_random_out_w 	= o_random_out_r;
-	last_random_w	= last_random_r;
 	clk_50hz_w 		= clk_50hz_r;
 	accumulator_w 	= accumulator_r + 1;
 	rnd_gen_cnt_w 	= rnd_gen_cnt_r;
 
 	// FSM
 	case(state_r)
-	S_IDLE: begin //idle
+	S_IDLE: begin // idle
 		if (i_start) begin
-			state_w = S_PROC; //before enter the proc state, run the first time
+			state_w = S_PROC; // before enter the proc state, run the first time
 			o_random_out_w 	= random_number[3:0];
-			last_random_w  	= o_random_out_r;
 			rnd_gen_cnt_w 	= 5'd0;
 		end
 	end
 
-	S_PROC: begin //running
+	S_PROC: begin // running
 		if (rnd_gen_cnt_r >= 5'd25) begin
 			state_w = S_IDLE;
 		end
 		else begin
 			if (clk_50hz_r >= rnd_gen_cnt_wait[rnd_gen_cnt_r]) begin
-				o_random_out_w 	= random_number[3:0];
-				last_random_w 	= o_random_out_r;
+				o_random_out_w = random_number[3:0] ^ (random_number[3:0] == o_random_out_r ? 4'd15 : 4'd0);
 				clk_50hz_w 		= 11'd0;
 				rnd_gen_cnt_w 	= rnd_gen_cnt_r + 1;
 			end
 			else begin
 				if (accumulator_r == 20'd0) begin
 					clk_50hz_w = clk_50hz_r + 1;
-					if (last_random_r == o_random_out_r) begin
-						o_random_out_w 	= random_number[3:0];
-						last_random_w  	= o_random_out_r;
-					end
 				end
 			end
 		end
@@ -77,7 +69,6 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin
 	if (!i_rst_n) begin
 		state_r        	<= S_IDLE;
 		o_random_out_r 	<= 4'd0;
-		last_random_r 	<= 4'd0;
 		clk_50hz_r 		<= 11'd0;
 		accumulator_r 	<= 20'd0;
 		rnd_gen_cnt_r 	<= 5'd0;
@@ -85,7 +76,6 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin
 	else begin
 		state_r        	<= state_w;
 		o_random_out_r 	<= o_random_out_w;
-		last_random_r 	<= last_random_w;
 		clk_50hz_r 		<= clk_50hz_w;
 		accumulator_r 	<= accumulator_w;
 		rnd_gen_cnt_r 	<= rnd_gen_cnt_w;
@@ -117,7 +107,7 @@ end
 always_ff @(posedge i_clk or negedge i_rst_n) begin
 	// reset
 	if (!i_rst_n) begin
-		random_r 	<= 16'h5CA7; //Magic Number
+		random_r 	<= 16'h5CA7; // Magic Number
 	end
 	else begin
 		random_r 	<= random_w;
