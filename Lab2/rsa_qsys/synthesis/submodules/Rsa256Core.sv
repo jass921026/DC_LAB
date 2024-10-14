8054c64fde8767464cc8b4f0236972594cdedd26
jass921026 <jass921026@gmail.com>
2024-10-14 23:02:21 +0800

    add qsys files

---
+++ Lab2/rsa_qsys/synthesis/submodules/Rsa256Core.sv
@@ -0,0 +1,258 @@
+module Rsa256Core 
+#(
+    parameter bitwidth = 256
+)
+(
+    input          			i_clk,
+    input         			i_rst,
+    input          			i_start,
+    input  [bitwidth-1:0] 	i_a, // cipher text y
+    input  [bitwidth-1:0] 	i_d, // private key
+    input  [bitwidth-1:0] 	i_n,
+    output [bitwidth-1:0] 	o_a_pow_d, // plain text x
+    output         			o_finished
+);
+
+// Define States and Parameters
+
+localparam S_IDLE 	= 2'd0;
+localparam S_CALC 	= 2'd1;
+localparam S_MONT	= 2'd2;
+localparam S_LSHIFT	= 2'd3;
+
+// Define Variables
+
+logic [1:0] 					state_w, 	state_r;
+logic [$clog2(bitwidth):0]	 	iter_w, 	iter_r;
+logic [bitwidth:0] 				t_w, 		t_r;
+logic [bitwidth-1:0] 			m_w, 		m_r;
+logic							finished_w,	finished_r;
+logic							montfin,	montfin2;
+logic							montstart_w, montstart_r;
+logic 							montstart2_w, montstart2_r;
+logic [bitwidth-1:0]			mont_a,		mont_b;
+logic [bitwidth-1:0]			mont_a2,	mont_b2;
+logic [bitwidth-1:0]			mont_result,mont_result2;
+
+// wiring and instantiation
+
+montgomery #(.bitwidth(bitwidth)) montgomery_inst (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .i_start(montstart_r),
+    .i_mdl(i_n),
+    .i_a(mont_a),
+    .i_b(mont_b),
+    .o_finished(montfin),
+    .o_result(mont_result)
+);
+montgomery #(.bitwidth(bitwidth)) montgomery_inst2 (
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .i_start(montstart2_r),
+    .i_mdl(i_n),
+    .i_a(mont_a2),
+    .i_b(mont_b2),
+    .o_finished(montfin2),
+    .o_result(mont_result2)
+);
+
+// Combintional Circuits
+
+assign o_a_pow_d 	= 	m_r;
+assign o_finished	=	finished_r;
+
+assign mont_a = m_r;
+assign mont_b = t_r;
+assign mont_a2 = t_r;
+assign mont_b2 = t_r;
+
+always_comb begin
+
+    // Unconditional Assignments
+    state_w = state_r;
+    iter_w 	= iter_r;
+    t_w 	= t_r;
+    m_w 	= m_r;
+    finished_w = finished_r;
+	montstart_w = montstart_r;
+	montstart2_w = montstart2_r;
+
+    case (state_r)
+        S_IDLE: begin
+            finished_w	= 0;
+            if (i_start) begin//before enter the lshift state, run the first time
+                state_w = S_LSHIFT;
+                iter_w	= 0;
+                t_w		= i_a;
+                m_w		= 1;
+            end
+        end
+        S_LSHIFT: begin
+            if (iter_r == bitwidth) begin
+                state_w	= S_CALC;
+                iter_w	= 0;
+            end
+            else begin
+                t_w		= ((t_r<<1)<i_n)?(t_r<<1):((t_r<<1)-i_n);
+                iter_w	= iter_r+1;
+            end
+        end
+        S_CALC: begin
+            if (iter_r	== bitwidth) begin
+                state_w		= S_IDLE;
+                finished_w	= 1;
+                iter_w		= 0;
+            end
+            else begin
+                if (i_d[iter_r] == 1) begin
+                    montstart_w	= 1;
+                end
+                state_w		= S_MONT;
+                montstart2_w	= 1;
+            end
+        end
+        S_MONT: begin
+            montstart_w	= 0;
+            montstart2_w	= 0;
+            if(montfin	==1) begin
+                m_w		= mont_result;
+            end
+            if(montfin2	==1) begin
+                t_w		= mont_result2;
+            end
+            if(montfin	== 1 && montfin2	== 1) begin				
+                state_w	= S_CALC;
+                iter_w	= iter_r+1;
+            end
+        end
+    endcase
+
+end
+
+
+// Sequential Circuits
+
+always_ff @(posedge i_clk or posedge i_rst) begin
+    // reset
+    if (i_rst) begin
+        state_r 	<= S_IDLE;
+        iter_r 		<= 0;
+        t_r 		<= 0;
+        m_r 		<= 1; // m = 1, plz check the montgomery algorithm
+        finished_r	<= 0;
+    end
+    else begin
+        state_r 	<= state_w;
+        iter_r 		<= iter_w;
+        t_r 		<= t_w;
+        m_r 		<= m_w;
+        finished_r	<= finished_w;
+    end
+end
+
+endmodule
+
+
+module montgomery
+// use module_name #(.PARAM(PARAM)) instance_name ( ... ); 
+// to instantiate a module with parameters to support variable bitwidth
+
+// This module only perform the montgomery once !!!
+#(
+    parameter bitwidth = 256
+)
+(
+    input          			i_clk,
+    input         			i_rst,
+    input          			i_start,
+    input  [bitwidth-1:0] 	i_mdl, 	// modulus
+    input  [bitwidth-1:0] 	i_a, 	// multiplier
+    input  [bitwidth-1:0] 	i_b,	// multiplicand
+    output         			o_finished,
+    output [bitwidth-1:0] 	o_result
+);
+
+// Define States and Parameters
+
+localparam S_IDLE = 2'd0;
+localparam S_CALC = 2'd1;
+
+// Define Variables
+logic [$clog2(bitwidth):0]	 	iter_w, 	iter_r;
+logic 							state_w, 	state_r;
+logic [bitwidth:0] 				m_w, 		m_r; // m might have carry bit
+logic [bitwidth+1:0]			m1;
+logic [bitwidth+1:0]			m2;
+logic							finished_w,	finished_r;
+
+// wiring
+assign o_result 	= 	m_r[bitwidth-1:0];
+assign o_finished	=	finished_r;
+
+// Combintional Circuits
+always_comb begin
+
+    // Unconditional Assignments  
+    state_w 	= state_r;
+    iter_w 		= iter_r;
+    m_w 		= m_r;
+    finished_w	= finished_r;
+	m1			= 0;
+	m2			= 0;
+    case (state_r)
+        S_IDLE: begin
+            if (i_start) begin
+                state_w 	=	S_CALC;
+                iter_w		=	0;
+                m_w			=	0;
+                finished_w	=	1'b0;
+            end
+        end
+        S_CALC: begin
+            if ( iter_r < bitwidth) begin
+                if ( i_a[iter_r]) begin
+                    m1	=	m_r+i_b;
+                end
+                else begin
+                    m1	=	m_r;
+                end
+                if ( m1[0]) begin
+                    m2	=	m1+i_mdl;
+                end
+                else begin
+                    m2	=	m1;
+                end
+                m_w		=	m2>>1;
+                iter_w	=	iter_r+1;
+            end
+            else begin
+                if ( m_r>=i_mdl) begin
+                    m_w		=	m_r-i_mdl;
+                end
+                finished_w	=	1'b1;
+                state_w		=	S_IDLE;
+            end
+        end
+    endcase
+end
+
+// Sequential Circuits
+always_ff @(posedge i_clk or posedge i_rst) begin
+    // reset
+    if (i_rst) begin
+        iter_r 		<= 0;
+        state_r 	<= S_IDLE;
+        m_r 		<= 0;
+        finished_r	<= 1'b0;
+    end
+    else begin
+        iter_r 		<= iter_w;
+        state_r 	<= state_w;
+        m_r 		<= m_w;
+        finished_r	<= finished_w;
+    end
+end	
+
+endmodule
+    
---
+++ Lab2/rsa_qsys/synthesis/submodules/Rsa256Wrapper.sv
@@ -0,0 +1,223 @@
+module Rsa256Wrapper 
+// and decoder for the RSA
+#(
+    parameter bitwidth = 256
+)
+(
+    input         avm_rst,
+    input         avm_clk,
+    output  [4:0] avm_address,
+    output        avm_read,
+    input  [31:0] avm_readdata,
+    output        avm_write,
+    output [31:0] avm_writedata,
+    input         avm_waitrequest
+);
+
+localparam RX_BASE     = 0*4;
+localparam TX_BASE     = 1*4;
+localparam STATUS_BASE = 2*4;
+localparam TX_OK_BIT   = 6;
+localparam RX_OK_BIT   = 7;
+
+// Define States and Parameters
+localparam S_GET_KEY_N = 0; 
+localparam S_GET_KEY_D = 1;
+localparam S_GET_DATA  = 2;
+localparam S_REQ_CALC  = 3;
+localparam S_WAIT_CALC = 4;
+localparam S_SEND_DATA = 5;
+
+// Define Variables
+
+// datas
+logic [bitwidth-1:0] n_r, n_w; // the public n 
+logic [bitwidth-1:0] d_r, d_w; // the private key
+logic [bitwidth-1:0] enc_r, enc_w; // the cipher text
+logic [bitwidth-1:0] dec_r, dec_w; // the plain text
+
+// states
+logic [2:0] state_r, state_w; // finite state machine
+logic [$clog2(bitwidth)-4:0] byte_cnt_r, byte_cnt_w; // count current IO byte
+
+// io 
+
+logic [4:0] avm_address_r, avm_address_w; // address access register
+logic avm_read_r, avm_read_w; // indicate this model is reading
+logic avm_write_r, avm_write_w; // indicate this model is writing
+
+// RSA core
+logic rsa_start_r, rsa_start_w;
+logic rsa_finished;
+logic [bitwidth-1:0] rsa_dec;
+
+assign avm_address      = avm_address_r;
+assign avm_read         = avm_read_r;
+assign avm_write        = avm_write_r;
+assign avm_writedata    = dec_r[bitwidth-9 -:8]; //the only thing to write is plain text
+
+Rsa256Core #(.bitwidth(256)) rsa256_core(
+    .i_clk(avm_clk),
+    .i_rst(avm_rst),
+    .i_start(rsa_start_r),
+    .i_a(enc_r),
+    .i_d(d_r),
+    .i_n(n_r),
+    .o_a_pow_d(rsa_dec),
+    .o_finished(rsa_finished)
+);
+
+task Reading;
+    begin
+        avm_read_w      = 1;
+        avm_write_w     = 0;
+    end
+endtask
+task Writing;
+    begin
+        avm_read_w      = 0;
+        avm_write_w     = 1;
+    end
+endtask
+
+always_comb begin
+    // Unconditional Assignments
+    n_w             = n_r;
+    d_w             = d_r;
+    enc_w           = enc_r;
+    dec_w           = dec_r;
+    avm_read_w      = avm_read_r;   
+    avm_write_w     = avm_write_r;
+    byte_cnt_w      = byte_cnt_r;
+    rsa_start_w     = rsa_start_r;
+    avm_address_w   = avm_address_r;
+    state_w         = state_r;
+
+    case (state_r)
+        S_GET_KEY_N: begin
+            Reading();
+            if (!avm_waitrequest) begin
+                if (avm_address_r == STATUS_BASE && avm_readdata[RX_OK_BIT]) begin
+                    avm_address_w = RX_BASE;
+                end
+                if (avm_address_r == RX_BASE) begin
+                    avm_address_w = STATUS_BASE;
+                    n_w[7:0] = avm_readdata[7:0];
+                    n_w[bitwidth-1 : 8] = n_r[bitwidth-9 : 0]; // shift left 8 bits
+                    if (byte_cnt_r == bitwidth/8 -1) begin
+                        // read finished
+                        byte_cnt_w = 0;
+                        state_w = S_GET_KEY_D;
+                    end
+                    else begin
+                        byte_cnt_w = byte_cnt_r + 1;
+                    end
+                end
+            end
+        end
+        S_GET_KEY_D: begin
+            Reading();
+            if (!avm_waitrequest) begin
+                if (avm_address_r == STATUS_BASE && avm_readdata[RX_OK_BIT]) begin
+                    avm_address_w = RX_BASE;
+                end
+                if (avm_address_r == RX_BASE) begin
+                    avm_address_w = STATUS_BASE;
+                    d_w[7:0] = avm_readdata[7:0];
+                    d_w[bitwidth-1 : 8] = d_r[bitwidth-9 : 0]; // shift left 8 bits
+                    if (byte_cnt_r == bitwidth/8 -1) begin
+                        // read finished
+                        byte_cnt_w = 0;
+                        state_w = S_GET_DATA;
+                    end
+                    else begin
+                        byte_cnt_w = byte_cnt_r + 1;
+                    end
+                end
+            end
+        end
+        S_GET_DATA: begin
+            Reading();
+            if (!avm_waitrequest) begin
+                if (avm_address_r == STATUS_BASE && avm_readdata[RX_OK_BIT]) begin
+                    avm_address_w = RX_BASE;
+                end
+                if (avm_address_r == RX_BASE) begin
+                    avm_address_w = STATUS_BASE;
+                    enc_w[7:0] = avm_readdata[7:0];
+                    enc_w[bitwidth-1 : 8] = enc_r[bitwidth-9 : 0]; // shift left 8 bits
+                    if (byte_cnt_r == bitwidth/8 -1) begin
+                        // read finished
+                        byte_cnt_w = 0;
+                        state_w = S_REQ_CALC;
+                    end
+                    else begin
+                        byte_cnt_w = byte_cnt_r + 1;
+                    end
+                end
+            end
+        end
+        S_REQ_CALC: begin
+            rsa_start_w = 1;
+            state_w = S_WAIT_CALC;
+        end
+        S_WAIT_CALC: begin
+            rsa_start_w = 0;
+            if (rsa_finished) begin
+                dec_w = rsa_dec;
+                state_w = S_SEND_DATA;
+            end
+        end
+        S_SEND_DATA: begin
+            //after finish, loop back to get data
+            if (!avm_waitrequest) begin
+                if (avm_address_r == STATUS_BASE && avm_readdata[TX_OK_BIT]) begin
+                    avm_address_w = TX_BASE;
+                    Writing();
+                end
+                if (avm_address_r == TX_BASE) begin
+                    avm_address_w = STATUS_BASE;
+                    Reading();
+                    dec_w[bitwidth-1 : 8] = dec_r[bitwidth-9 : 0]; // shift left 8 bits
+                    if (byte_cnt_r == bitwidth/8 -2) begin // only 31 bytes are required
+                        // write finished
+                        byte_cnt_w = 0;
+                        state_w = S_GET_DATA;
+                    end
+                    else begin
+                        byte_cnt_w = byte_cnt_r + 1;
+                    end
+                end
+            end
+        end
+    endcase
+end
+
+always_ff @(posedge avm_clk or posedge avm_rst) begin
+    // reset
+    if (avm_rst) begin
+        n_r             <= 0;
+        d_r             <= 0;
+        enc_r           <= 0;
+        dec_r           <= 0;
+        avm_read_r      <= 0;
+        avm_write_r     <= 0;
+        byte_cnt_r      <= 0;
+        rsa_start_r     <= 0;
+        avm_address_r   <= STATUS_BASE;
+        state_r         <= S_GET_KEY_N;
+    end 
+    else begin
+        n_r             <= n_w;
+        d_r             <= d_w;
+        enc_r           <= enc_w;
+        dec_r           <= dec_w;
+        avm_read_r      <= avm_read_w;
+        avm_write_r     <= avm_write_w;
+        byte_cnt_r      <= byte_cnt_w;
+        rsa_start_r     <= rsa_start_w;
+        avm_address_r   <= avm_address_w;
+        state_r         <= state_w;
+    end
+end
+endmodule
