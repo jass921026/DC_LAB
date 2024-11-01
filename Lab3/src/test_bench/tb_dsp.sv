ed5be90f1b862f1f1161ea840af53cbe9b1a2dd5
swear01 <stanley.yellow1@gmail.com>
2024-11-01 20:45:56 +0800

    TB Finish

--- Lab3/src/test_bench/tb_dsp.sv
+++ Lab3/src/test_bench/tb_dsp.sv
@@ -3,42 +3,90 @@
 module tb;
 	localparam CLK = 10;
 	localparam HCLK = CLK/2;
+	localparam memsize = 1048576;
 
-	logic clk, start_cal, fin, rst;
+	logic clk, daclrck;
+	logic rst, start, pause, stop, mode;
+	logic [2:0] speed;
+	logic [15:0] sram_data[0:memsize-1], dac_data[0:memsize-1], golden[0:memsize-1];
+	logic [15:0] sram_block, dac_block;
+	logic [19:0] sram_addr;
 	initial clk = 0;
+	initial daclrck = 0;
 	always #HCLK clk = ~clk;
-	logic [255:0] encrypted_data, decrypted_data;
-	logic [247:0] golden;
-	integer fp_e, fp_d;
+	always #4e4*CLK daclrck = ~daclrck;
+	
 
-	Rsa256Core core(
-		.i_clk(clk),
-		.i_rst(rst),
-		.i_start(start_cal),
-		.i_a(encrypted_data),
-		.i_d(256'hB6ACE0B14720169839B15FD13326CF1A1829BEAFC37BB937BEC8802FBCF46BD9),
-		.i_n(256'hCA3586E7EA485F3B0A222A4C79F7DD12E85388ECCDEE4035940D774C029CF831),
-		.o_a_pow_d(decrypted_data),
-		.o_finished(fin)
+	AudDSP dsp(
+		.rst_n(~rst),
+		.clk(clk),
+		.start(start),
+		.pause(pause),
+		.stop(stop),
+		.speed(speed),
+		.interpolation_mode(mode),
+		.daclrck(daclrck),
+		.sram_data(sram_block),
+		.dac_data(dac_block),
+		.sram_addr(sram_addr)
 	);
 
 	initial begin
-		$fsdbDumpfile("lab2.fsdb");
+		$fsdbDumpfile("lab3_dsp.fsdb");
 		$fsdbDumpvars;
-		fp_e = $fopen("../pc_python/golden/enc1.bin", "rb");
-		fp_d = $fopen("../pc_python/golden/dec1.txt", "rb");
+		fd = $fopen("./dsp_testdata.txt", "rb");
+		fg = $fopen("./dsp_golden.txt", "rb");
+
+		// Read test data
+		for (int i = 0; i < memsize; i++) begin
+			$fscanf(fd, "%h", sram_data[i]);
+		end
+			
+
 		rst = 1;
-		#(2*CLK)
+		#(3*CLK)
 		rst = 0;
+
+
+		for (int iter = 0 ; iter < 10; iter++) begin
+			$fscanf("%d %d" ,speed, mode);
+			// read golden data
+			for (int i = 0; i < memsize; i++) begin
+				$fscanf(fg, "%h", golden[i]);
+			end
+			start = 1;
+			#(CLK)
+			start = 0;
+
+			let max(a,b) = (a > b) ? a : b;
+			for (int i = 0; i < memsize>>max(speed-3,0); i++) begin
+				@(posedge daclrck);
+				// collect output data
+				dac_data[i] = dac_block;
+			end
+			// TODO: add random pause
+
+			stop = 1;
+			#(CLK)
+			stop = 0;
+
+			// compare result
+			for (int i = 0; i < memsize>>max(speed-3,0); i++) begin
+				if (dac_data != golden[i]) begin
+					$display("Error at %d: %h != %h", i, dac_data[i], golden[i]);
+					$finish;
+				end
+			end
+
+		end
+
 		for (int i = 0; i < 5; i++) begin
 			for (int j = 0; j < 10; j++) begin
 				@(posedge clk);
 			end
 			$fread(encrypted_data, fp_e);
 			$fread(golden, fp_d);
-			$display("=========");
-			$display("enc  %2d = %64x", i, encrypted_data);
-			$display("=========");
+
 			start_cal <= 1;
 			@(posedge clk)
 			encrypted_data <= 'x;
@@ -50,6 +98,12 @@ module tb;
 			$display("=========");
 		end
 		$finish;
+	end
+
+
+	always begin //work sram
+		#(CLK/2)
+		sram_block = sram_data[sram_addr];
 	end
 
 	initial begin
