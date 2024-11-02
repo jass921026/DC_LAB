187b55aea1c251552a34e149735cbc3d8038e047
swear01 <stanley.yellow1@gmail.com>
2024-11-02 08:24:45 +0800

    neg

--- Lab3/src/test_bench/tb_dsp.sv
+++ Lab3/src/test_bench/tb_dsp.sv
@@ -72,9 +72,10 @@ module tb;
             // endfunction
 
             for (int i = 0; i < memsize>>(speed-3 > 0 ? speed-3 : 0); i++) begin
-                @(daclrck);
+                @(posedge clk);
                 // collect output data
                 dac_data[i] = dac_block;
+                @negedge daclrck;
             end
             // TODO: add random pause
 
@@ -83,9 +84,9 @@ module tb;
             stop = 0;
 
             // compare result
-            $display("Data: %h %h %h %h", dac_data[0], dac_data[1], dac_data[2], dac_data[3]);
+            $display("Obtain Data: %h %h %h %h", dac_data[0], dac_data[1], dac_data[2], dac_data[3]);
             for (int i = 0; i < memsize>>(speed-3 > 0 ? speed-3 : 0); i++) begin
-                if (dac_data[i] != golden[i]) begin
+                if (dac_data[i] !== golden[i]) begin
                     $display("Error at %d: %h != %h", i, dac_data[i], golden[i]);
                     $finish;
                 end
