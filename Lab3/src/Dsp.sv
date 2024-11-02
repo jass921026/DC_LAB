70ae2bd9fd5d7b640eae789ea596692fa32e4abe
swear01 <stanley.yellow1@gmail.com>
2024-11-02 09:38:13 +0800

    fix

--- Lab3/src/Dsp.sv
+++ Lab3/src/Dsp.sv
@@ -17,7 +17,7 @@ logic [ 2:0] state_r, state_w;
 logic        prev_daclrck_r, prev_daclrck_w;
 logic [15:0] out_data_r, out_data_w;
 logic [15:0] prev_data_r, prev_data_w;
-logic [2:0] interpolation_counter_r, interpolation_counter_w;
+logic [2:0] interpolation_cnt_r, interpolation_cnt_w;
 
 parameter S_IDLE      = 0;
 parameter S_PAUSE     = 1;
@@ -35,7 +35,7 @@ always_comb begin
     prev_daclrck_w = prev_daclrck_r;
     prev_data_w = prev_data_r;
     out_data_w = out_data_r;
-    interpolation_counter_w = interpolation_counter_r;
+    interpolation_cnt_w = interpolation_cnt_r;
 
 
     case (state_r)
@@ -68,18 +68,19 @@ always_comb begin
                     prev_data_w = i_sram_data;
                 end
                 else begin //slow down
-                    if (i_interpolation_mode == 0) begin
-                    end
-                    else begin // no interpolation
-                        if (interpolation_counter_r === 1<<(-i_speed+3)-1) begin
-                            interpolation_counter_w = 0;
+                    if (interpolation_cnt_r == 1<<(-i_speed+3)-1) begin
+                            interpolation_cnt_w = 0;
                             addr_w = addr_r + 1 ;
                             prev_data_w = i_sram_data;
                         end
-                        else begin // linear interpolation
-                            interpolation_counter_w = interpolation_counter_r + 1;
-                        end
-                        out_data_w = prev_data_r + (i_sram_data - prev_data_r) * interpolation_counter_r / (1<<(-i_speed+3));
+                    else begin 
+                        interpolation_cnt_w = interpolation_cnt_r + 1;
+                    end
+                    if (i_interpolation_mode == 0) begin // no interpolation
+                        out_data_w = prev_data_r;
+                    end
+                    else begin // linear interpolation
+                        out_data_w = prev_data_r + (i_sram_data - prev_data_r) * interpolation_cnt_r / (1<<(-i_speed+3));
                     end
                 end
             end
@@ -92,7 +93,7 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin
     if (!i_rst_n) begin
         prev_data_r <= i_sram_data ;
         prev_daclrck_r <= i_daclrck;
-        interpolation_counter_r <= 0;
+        interpolation_cnt_r <= 0;
         state_r <= S_IDLE;
         addr_r <= 0;
         out_data_r <= 0;
@@ -100,7 +101,7 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin
     else begin
         prev_data_r <= prev_data_w;
         prev_daclrck_r <= prev_daclrck_w;
-        interpolation_counter_r <= interpolation_counter_w;
+        interpolation_cnt_r <= interpolation_cnt_w;
         state_r <= state_w;
         addr_r <= addr_w;
         out_data_r <= out_data_w;
