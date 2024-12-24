891519f80b24f6fe68ce104d7174b171a67f6a21
jass921026 <jass921026@gmail.com>
2024-12-24 17:08:52 +0800

    temp

---
+++ Final/src/mouse/mouse.sv
@@ -0,0 +1,14 @@
+// Module for mouse communication
+
+module Mouse
+(
+    input clk,  // system clock
+    input rst,  // system reset
+    inout ps2_clk,  // mouse clock
+    inout ps2_data, // mouse data
+    output [7:0] data,  // module output 
+);
+
+// Todo: Send reset, Send enable streaming, receive streaming input
+
+endmodule
