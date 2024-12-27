07287b39598a79259eadfac92578d725b6dea7d9
swear01 <stanley.yellow1@gmail.com>
2024-12-27 10:55:49 +0800

    change vs logic to register

--- Final2/src/magic/magic_compare.sv
+++ Final2/src/magic/magic_compare.sv
@@ -6,9 +6,8 @@ module Magic_compare
     input [15:0] son_b, 
     input [15:0] mom_a, // 分母
     input [15:0] mom_b,
-    output [3:0] idx_won,
-    output [15:0] son_won,
-    output [15:0] mom_won
+    output [3:0] idx_won
+
 );
 
     logic [31:0] prod_a, prod_b;
@@ -17,7 +16,5 @@ module Magic_compare
     assign prod_b = 32'(son_b) * mom_a;
 
     assign idx_won = (prod_a >= prod_b) ? idx_a : idx_b;
-    assign son_won = (prod_a >= prod_b) ? son_a : son_b;
-    assign mom_won = (prod_a >= prod_b) ? mom_a : mom_b;
-
+    
 endmodule
